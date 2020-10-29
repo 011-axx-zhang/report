1729ba5e20aeee19b5606ebefedea9c0a401c914
zt <tz.zhang@timanetworks.com>
2020-10-29 18:38:04 +0800

    10-29日修改

---
+++ VehicleManage/VehicleManage/MainFunction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VehicleManage
+//
+//  Created by 亭子 on 2020/10/29.
+//  Copyright © 2020 zt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
